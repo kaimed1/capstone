419e1ef77031f62a7c46d464dd00f5a1d6c83237
Karly1234 <98110883+Karly1234@users.noreply.github.com>
2024-09-18 11:56:06 -0500

    Styled and created functionality

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,10 @@
 import football from './football.png';
 import georgia from './georgia.png';
 import ohio from './ohio.png';
-import alabama from './Alabama.png';
+import ou from './ou.png';
+import gator from './gator.png'
+import kansas from './Kansas.png'
+import rabbit from './rabbits.png'
 import './App.css';
 import React, {useState} from 'react';
 
@@ -22,11 +25,14 @@ function App() {
       <img src={football} alt="logo" class = 'center-image' />
       <img src={georgia} alt="Left Image" class="left-image"></img>
       <img src={ohio} alt="Left Image" class="left-image-second"></img>
-      <img src={alabama} alt="Left Image" class="left-image-last"></img>
-  
+      <img src={ou} alt="Left Image" class="left-image-last"></img>
+      <img src={gator} alt="Right Image" class="right-image"></img>
+      <img src={kansas} alt="Right Image" class="right-image-second"></img>
+      <img src={rabbit} alt="Right Image" class="right-image-last"></img>
+
       <input type = "text" id ="textbox" class = 'textbox' value = {inputValue} onChange = {handleInputCharge} placeholder = "Team 1"/>
       <input type = "text" id ="textbox" class = 'second-textbox' value = {inputValue} onChange = {handleInputCharge} placeholder = "Team 2"/>
-      
+
       </header>
     </div>
   );
