7547a9c7697c2c05d32d9ecf436446353a84b153
Jonas Metzger <Jonas.A.Metzger-1@ou.edu>
2024-10-08 12:32:08 -0500

    Added random forest prediction to API - New API route random_forest_2023 - Uses 2023 end-of-season data to predict a new game

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("random", views.random_prediction, name="random_prediction"),\
+    path("random", views.random_prediction, name="random_prediction"),
+    path("random_forest_2023", views.random_forest_prediction, name="random_forest_2023")
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse, JsonResponse
+
 from api.methods.random_prediction import random_prediction as random_prediction_method
+from api.methods.random_forest_prediction import random_forest_prediction as random_forest_prediction_method
+from api.helpers.get_end_of_season_standings import get_end_of_season_standings
+
+end_of_season_standings = get_end_of_season_standings()
 
 def index(request):
     return HttpResponse("Index")
 
-
+# Just randomly choose a winner and loser lol
 def random_prediction(request):
 
     # Get home team param
@@ -23,3 +28,39 @@ def random_prediction(request):
     }
 
     return JsonResponse(res)
+
+# Predict using random forest classifier model (first method we did)
+def random_forest_prediction(request):
+     # Get home team param
+    home_team = request.GET.get("home").replace("_", " ")
+
+    # Get away team param
+    away_team = request.GET.get("away").replace("_", " ")
+
+    home_team_standing = ""
+    away_team_standing = ""
+
+    try:
+        home_team_standing = end_of_season_standings[home_team]
+        away_team_standing = end_of_season_standings[away_team]
+    except:
+        return JsonResponse({
+            "error": "Invalid team name"
+        })
+    
+    try:
+        winner_name, loser_name, winner_score, loser_score = random_forest_prediction_method(home_team_standing, away_team_standing)
+
+        res = {
+            "winner_name": winner_name,
+            "loser_name": loser_name,
+            "winner_score": winner_score,
+            "loser_score": loser_score,
+            "error": None
+        }
+        
+        return JsonResponse(res)
+    except:
+        return JsonResponse({
+            "error": "Error occured when predicting game outcome"
+        })
