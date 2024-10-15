92f25ba77f5d21d5d256282de18d5bd23e90b3ad
nathanstephani12 <nathanstephani12@gmail.com>
2024-10-15 12:15:10 -0500

    Finalize training data structure
    Finalize training data composition in Schedule_Stats.csv.
    Corrected Teams.csv School column to match
    sports-reference for future data integration.

--- backend/src/fbs_schedule_with_stats.py
+++ backend/src/fbs_schedule_with_stats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 stat_order = [
     'Rk',
@@ -24,9 +25,9 @@ stat_order = [
 
 # Combines Teams.csv with 2023FBSAdvanced.csv for only teams included in 2023FBSAdvanced.csv
 def ChangeStatsFile():
-    stats_df = pd.read_csv('../data/2023FBSAdvanced.csv')
+    stats_df = pd.read_csv('backend/data/2023FBSAdvanced.csv')
 
-    teams_df = pd.read_csv('../data/Teams.csv')
+    teams_df = pd.read_csv('backend/data/Teams.csv')
 
     merged_df = pd.merge(stats_df, teams_df[['Conference', 'team_id', 'School']], left_on='School', right_on='School', how='left')
 
@@ -36,9 +37,9 @@ def ChangeStatsFile():
 
 # Translates team names in Schedule.csv into their team_id from Teams.csv
 def ChangeScheduleFile():
-    schedule_df = pd.read_csv('../data/Training_Schedule.csv')
+    schedule_df = pd.read_csv('backend/data/Training_Schedule.csv')
 
-    team_df = pd.read_csv('../data/Teams.csv')
+    team_df = pd.read_csv('backend/data/Teams.csv')
 
     schedule_df = schedule_df.merge(team_df, left_on='Team', right_on='Team', how='left')
 
@@ -58,7 +59,7 @@ def ChangeScheduleFile():
 
     return schedule_df
 
-# Adds advanced stats from Combined_2023_Team_Stats.csv with Schedule.csv and filters out any matching including FCS teams
+# Adds advanced stats from Combined_2023_Team_Stats.csv with Schedule.csv and filters out any matchups including FCS teams
 def AddStatsToSchedule(stats_df, schedule_df):
     stats_df.drop(columns=['Rk'], inplace=True)
 
@@ -70,22 +71,17 @@ def AddStatsToSchedule(stats_df, schedule_df):
 
     schedule_df.dropna(subset=['Conference'], inplace=True)
 
-    schedule_df.drop(columns=['School', 'team_id', 'Conference'], inplace=True) #Eventually add columns back in
-
     pd.set_option('future.no_silent_downcasting', True)
 
     schedule_df['Result'] = schedule_df['Result'].replace({'W': 1, 'L': 0})
 
-    schedule_df['W'] = schedule_df['W'].astype('int')
+    schedule_df.drop(columns=['School', 'team_id', 'Conference', 'Date', 'Score', 'Opponent Score', 'AP Rank', 'W', 'L'], inplace=True)
 
-    schedule_df['L'] = schedule_df['L'].astype('int')
-
-    schedule_df['AP Rank'] = schedule_df['AP Rank'].fillna('-1').astype('int')
-
-    schedule_df.to_csv('../data/Schedule_Stats.csv', index=True, index_label='game_id')
-
+    return schedule_df
 
 stats_df = ChangeStatsFile()
 schedule_df = ChangeScheduleFile()
 
-AddStatsToSchedule(stats_df, schedule_df)
+merged_df = AddStatsToSchedule(stats_df, schedule_df)
+
+merged_df.to_csv('backend/data/Schedule_Stats.csv', index=True, index_label='game_id')
