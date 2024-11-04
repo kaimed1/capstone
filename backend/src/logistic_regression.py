61a151be884245b28a2bd5cb31a8f602173231b9
nathanstephani12 <97989452+nathanstephani12@users.noreply.github.com>
2024-11-04 14:08:02 -0600

    Nathan3/logistic regression (#17)
    * Add basic logistic regression model
    
    Add basic logistic regression model with almost
    default parameters. Very similar structure to linear
    regression model.
    
    * Edited Logistic Regression parameters
    
    Edited parameters of Logistic Regression model to reach >90%
    accuracy. Also dropped Team and Opponent id number from calculation,
    which did not affect model accuracy but reduced computation.
    
    * Allow saving Log Reg model
    
    Add saving function to Log Reg model
    with joblib and saved model in data/trained_models

---
+++ backend/src/logistic_regression.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import joblib
+
+def ReadAndFormatData(file_path):
+    # Load the data
+    df = pd.read_csv(file_path)
+
+    # Split the data into features and target
+    features = df.drop(columns=['game_id', 'Team', 'Opponent', 'Result'])
+    target = df['Result']
+
+    # Create feature differences between every two rows (team matchups)
+    team_a_stats = features.iloc[::2].reset_index(drop=True)
+    team_b_stats = features.iloc[1::2].reset_index(drop=True)
+    diff_stats = team_a_stats - team_b_stats  # Calculate the stat differences
+
+    # Corresponding outcomes for the matchups
+    game_outcomes = target.iloc[::2].reset_index(drop=True)  # Only take every other outcome
+
+    return diff_stats, game_outcomes
+
+
+def TrainModel(diff_stats, game_outcomes):
+    # Split the data into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(diff_stats, game_outcomes, test_size=0.2, random_state=23)
+
+    # Train the logistic regression model
+    log_reg_model = LogisticRegressionCV(max_iter=2000, fit_intercept=True, solver='liblinear', refit=True) # max_iter set to 2000 becasue model does not converge within default value of 100
+    log_reg_model.fit(X_train, y_train)
+    return log_reg_model, X_test, y_test
+
+def TestModel(log_reg_model, X_test, y_test):
+    # Make predictions
+    y_pred = log_reg_model.predict(X_test)  # Continuous predictions
+
+    # Calculate accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f'Logistic Regression Model Accuracy: {accuracy:.5f}')
+
+def SaveModel(log_reg_model):
+    # Save the logistic regression model using joblib
+    model_filename = 'backend/data/trained_models/logistic_regression_model.pkl'
+    joblib.dump(log_reg_model, model_filename)
+    print(f'Model saved to {model_filename}')
+
+def main():
+    # Path to data to read in
+    file_path = 'backend/data/Schedule_Stats.csv'
+
+    # Preparing data for training
+    stats, outcomes = ReadAndFormatData(file_path)
+
+    # Train and save model and test data
+    model, x, y = TrainModel(stats, outcomes)
+
+    # Test and evaluate accuracy
+    TestModel(model, x, y)
+
+    # Save model
+    SaveModel(model)
+
+if __name__ == "__main__":
+    main()
