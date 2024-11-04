cc7269dd90265e578c2a4bc3882d5d289408cd09
Preston <proesslet@gmail.com>
2024-11-04 10:22:03 -0600

    created page for predictions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import Bootstrap Components
+import Container from 'react-bootstrap/Container';
+
+// Import Components
 import NavBar from './components/NavBar';
+
+// Import Pages
+import Predictions from './pages/Predictions';
 
 function App() {
   return (
     <Router>
     <NavBar /> {/* Navbar is outside Routes so it appears on all pages */}
+    <Container>
     <Routes>
       <Route path="/" element={<h1>home</h1>} />
-      <Route path="/predictions" element={<h1>predictions</h1>} />
+      <Route path="/predictions" element={<Predictions />} />
     </Routes>
+    </Container>
   </Router>
   );
 }
---
+++ frontend/src/pages/Predictions.js
@@ -0,0 +1,9 @@
+function Predictions() {
+  return (
+    <div className="Predictions">
+      <h1>Predictions</h1>
+    </div>
+  );
+}
+
+export default Predictions;
