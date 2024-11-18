8ea1f6acc5979a742ddbe24d4efb446d6be67948
Metzark <jmetzger03@outlook.com>
2024-11-18 16:54:08 -0600

    Added random_forest_2 model to api: - Add route for random_forest_2 model

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("random", views.random_prediction, name="random_prediction"),
     path("random_forest", views.random_forest_prediction, name="random_forest"),
+    path("random_forest_2", views.random_forest_2_prediction, name="random_forest_2"),
     path("decision_tree", views.decision_tree_prediction, name="decision_tree"),
     path("chatgpt", views.chatgpt_prediction , name="chatgpt"),
     path("linear", views.linear_prediction, name="linear_prediction"),
